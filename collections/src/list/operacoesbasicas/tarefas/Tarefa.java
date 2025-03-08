b5aa2ec0f5dce3c28cd558222eafdce40eb2fed1
Pedro Ballastreri <gballastreri@gmail.com>
2025-03-08 14:20:18 -0300

    Trabalhando com List em java

---
+++ collections/src/list/operacoesbasicas/tarefas/Tarefa.java
@@ -0,0 +1,18 @@
+package list.operacoesbasicas.tarefas;
+
+public class Tarefa {
+    private String destricao;
+
+    public Tarefa(String destricao) {
+        this.destricao = destricao;
+    }
+
+    public String getDestricao() {
+        return destricao;
+    }
+
+    public String toString() {
+        return destricao;
+    }
+
+}
